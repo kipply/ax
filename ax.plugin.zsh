fe1bb85ed67e3606f776f8a86518886f13f321db
Carol Chen <hello@carolchen.me>
2020-07-13 22:54:43 -0400

    better

--- ax.plugin.zsh
+++ ax.plugin.zsh
@@ -2,10 +2,9 @@ function add-ax() {
   local _BUFFER="$BUFFER";
   [[ $_BUFFER = ax* ]] || [[ $_BUFFER = "" ]] || _BUFFER="ax $BUFFER";
   echo ''
-  ZLE_QAL_LAST="$BUFFER"
+  print -s "$BUFFER"
   eval $_BUFFER
   BUFFER=""
-  ZLE_QAL_STATUS=$?
   zle reset-prompt
 }
 zle -N accept-line add-ax
