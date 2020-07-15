bfed9e338f833b8489949000cfeb5c818e757076
Carol Chen <hello@carolchen.me>
2020-07-14 22:36:28 -0400

    yolo

--- ax.plugin.zsh
+++ ax.plugin.zsh
@@ -1,10 +1,26 @@
-function add-ax() {
-  local _BUFFER="$BUFFER";
-  [[ $_BUFFER = ax* ]] || [[ $_BUFFER = "" ]] || _BUFFER="ax $BUFFER";
-  echo ''
-  print -s "$BUFFER"
-  eval $_BUFFER
-  BUFFER=""
-  zle reset-prompt
+ax_ignore_types=("vi ssh man")
+ax_min_duration=1
+
+ax_pre() {
+  ax start
+  ax_last_exec=$(date +%s)
+  ax_cmd=$1
 }
-zle -N accept-line add-ax
+
+ax_post() {
+	if [ -z "$ax_last_exec" ]
+	then
+    ax_last_exec=$(date +%s)
+  fi
+
+  local duration=$(($(date +%s) - $ax_last_exec))
+  local cmd_type=$(echo $ax_cmd | awk '{print $1;}')
+
+  if [[ "$duration" -gt "$ax_min_duration" && ! " ${ax_ignore_types[@]} " =~ " ${cmd_type} " ]]; then
+    echo "hi"
+    ax end
+  fi
+}
+
+add-zsh-hook preexec ax_pre
+add-zsh-hook precmd ax_post
